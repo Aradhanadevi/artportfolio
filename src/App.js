417caa18142b9fb47a761dbf35264b6c07103bda
Bhagyashreeba Jadeja <bhagyashreejadeja14@gmail.com>
2025-11-01 23:18:03 +0530

    app js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import AnimationGrid from "./components/AnimationGrid";
+
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+
+      <AnimationGrid />
     </>
   );
 }
