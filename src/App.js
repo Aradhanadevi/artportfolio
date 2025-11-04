19cf3d54f0d9d99e82258013dde0f21d1baa6ab3
Aradhanadevi <aradhanajadeja81@gmail.com>
2025-11-04 21:30:49 +0530

    .

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import AnimationGrid from "./components/AnimationGrid";
+import AboutMe from "./components/AboutMe";
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Hero />
 
       <AnimationGrid />
+      <AboutMe />
     </>
   );
 }
