2dc87ce2618ce780edcd4dac38c6da69e27f9001
Aradhanadevi <aradhanajadeja81@gmail.com>
2025-11-05 22:01:50 +0530

    .

--- src/App.js
+++ src/App.js
@@ -3,16 +3,16 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import AnimationGrid from "./components/AnimationGrid";
 import AboutMe from "./components/AboutMe";
-
+import ToolsSection from "./components/ToolsSection";
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
-
       <AnimationGrid />
       <AboutMe />
+      <ToolsSection />
     </>
   );
 }
