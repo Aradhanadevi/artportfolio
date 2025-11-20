81f09b24f55c8822cf1355e639b6782ca5ee3852
Aradhanadevi <aradhanajadeja81@gmail.com>
2025-11-20 23:33:03 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import AnimationGrid from "./components/AnimationGrid";
@@ -7,10 +7,23 @@ import ToolsSection from "./components/ToolsSection";
 import Gallery from "./components/Gallery";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+import LoadingScreen from "./components/LoadingScreen";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000); // 2 seconds
+  }, []);
+
   return (
-    <>
+   <>
+  {loading ? (
+    <LoadingScreen />
+  ) : (
+    <div className="fade-in">
       <Navbar />
       <Hero />
       <AnimationGrid />
@@ -19,7 +32,10 @@ function App() {
       <Gallery />
       <Contact />
       <Footer />
-    </>
+    </div>
+  )}
+</>
+
   );
 }
 
