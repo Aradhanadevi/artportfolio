9cfbab9f9c77035be3b19494e9a33fb6c246f3ca
Aradhanadevi <aradhanajadeja81@gmail.com>
2025-12-09 16:09:02 +0530

    navbar-update

---
+++ src/components/Navbar.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "./Navbar.css";
+
+const Navbar = () => (
+  <nav className="navbar">
+    {/* LOGO */}
+    <div className="logo">Aradhana</div>
+
+    {/* NAV LINKS */}
+    <ul className="nav-links">
+      <li>
+        <a href="#home">Home</a>
+      </li>
+      <li>
+        <a href="#projects">Projects</a>
+      </li>
+      <li>
+        <a href="#about">About</a>
+      </li>
+      <li>
+        <a href="#gallery">Gallery</a>
+      </li>
+      <li>
+        <a href="#contact" className="contact-btn">
+          Contact
+        </a>
+      </li>
+    </ul>
+  </nav>
+);
+
+export default Navbar;
