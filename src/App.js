195269b32189a5cbae969be933c55f8889941e75
Aradhanadevi <aradhanajadeja81@gmail.com>
2026-01-19 22:56:49 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
+
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import AnimationGrid from "./components/AnimationGrid";
@@ -8,34 +9,32 @@ import Gallery from "./components/Gallery";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import LoadingScreen from "./components/LoadingScreen";
+import ProjectStory from "./components/ProjectStory";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    setTimeout(() => {
-      setLoading(false);
-    }, 2000); // 2 seconds
-  }, []);
-
   return (
     <>
-      {loading ? (
-        <LoadingScreen />
-      ) : (
-        <div className="fade-in cosmic-wrapper">
-          <div className="global-stars"></div>
-          <div className="global-stars2"></div>
-          <Navbar />
-          <Hero />
-          <AnimationGrid />
-          <AboutMe />
-          <ToolsSection />
-          <Gallery />
-          <Contact />
-          <Footer />
-        </div>
-      )}
+      <Routes>
+        {/* HOME */}
+        <Route
+          path="/"
+          element={
+            <>
+              <Navbar />
+              <Hero />
+              <AnimationGrid />
+              <AboutMe />
+              <ToolsSection />
+              <Gallery />
+              <Contact />
+              <Footer />
+            </>
+          }
+        />
+
+        {/* PROJECT STORY */}
+        <Route path="/projects/:slug" element={<ProjectStory />} />
+      </Routes>
     </>
   );
 }
